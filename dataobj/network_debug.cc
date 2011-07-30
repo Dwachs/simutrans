93147cf7670fd83e85cd66e923a883cf1a3423da
Dwachs <Dwachs@gmx.net>
2011-07-30 10:30:39 +0200

    ADD output into desync.log

--- dataobj/network_debug.cc
+++ dataobj/network_debug.cc
@@ -2,6 +2,12 @@
 #include "network_packet.h"
 #include "network_socket_list.h"
 #include "umgebung.h"
+#include <stdio.h>
+#ifdef _MSC_VER
+#include <direct.h>
+#else
+#include <unistd.h>
+#endif
 
 slist_tpl<nwc_debug_t::node_t*> nwc_debug_t::info;
 uint32 nwc_debug_t::max_nodes = 1;
@@ -10,7 +16,7 @@ void network_debug_desync(uint32 check_failed_sync_step, cbuffer_t &buf)
 {
 	typedef nwc_debug_t::node_t node_t;
 	SOCKET sock = socket_list_t::get_server_connection_socket();
-	buf.printf("Check failed at ss=%d<br>\n", check_failed_sync_step);
+	buf.printf("Check failed at sync-step=%d<br>", check_failed_sync_step);
 	slist_tpl<nwc_debug_t::node_t*> &info = nwc_debug_t::info;
 	// check the checksums in the list
 	node_t *first_failed = NULL;
@@ -20,7 +26,7 @@ void network_debug_desync(uint32 check_failed_sync_step, cbuffer_t &buf)
 		node_t *node = *iter;
 		// request info		
 		nwc_debug_t nwc(nwc_debug_t::get_chk, node->sync_step);
-		dbg->message("network_debug_desync", "request ss=%d", node->sync_step);
+		dbg->message("network_debug_desync", "request sync-step=%d", node->sync_step);
 		if(!nwc.send(sock)) {
 			goto send_error;
 		}
@@ -74,67 +80,87 @@ void network_debug_desync(uint32 check_failed_sync_step, cbuffer_t &buf)
 		if(!nwc.send(sock)) {
 			goto send_error;
 		}
-		nwc_debug_t *nwd = NULL;
-		// wait for nwc_debug_t, ignore other commands
-		for(uint8 i=0; i<5; i++) {
-			network_command_t* nwc = network_check_activity( NULL, 10000 );
-			if (nwc  &&  nwc->get_id() == NWC_DEBUG) {
-				nwd = (nwc_debug_t*)nwc;
-				break;
-			}
-			delete nwc;
-		}
-		if(nwd == NULL) {
-			buf.append("ERR: server did not respond<br>\n");
-			return;
-		}
-		buf.printf("<br><h1>Log message of sync-step %d</h1><br>", first_failed->sync_step);
-		// pointer to start of current messages
-		const char* mc = (const char*)(first_failed->buf);
-		const char* ms = (const char*)(*nwd->pbuf);
-		// now loop through the strings
-		const char* pc = mc;
-		const char* ps = ms;
-		bool equal = true;
-		bool eol = false;
-		while((*pc)  ||  (*ps)) {
-			// compare
-			if (equal  &&  (*ps)!=(*pc)) {
-				equal = false;
-			}
-			// advance till end of string / end of line if necessary
-			if (*ps  &&  *ps < 32) {
-				eol = true;
-				for (; (*pc)  &&  (*pc)>=32; pc++) {}
-			}
-			else if  (*pc  &&  *pc < 32) {
-				eol = true;
-				for (; (*ps)  &&  (*ps)>=32; ps++) {}
-			}
-			// output
-			if (eol) {
-				if (equal) {
-					std::string str_c(mc, pc-mc);
-					buf.printf("%s<br>", str_c.c_str());
-				}
-				else {
-					std::string str_c(mc, pc-mc);
-					buf.printf("<em>&gt;&gt;client&gt;&gt;</em><br><em>%s</em><br>", str_c.c_str());
-					std::string str_s(ms, ps-ms);
-					buf.printf("====<br><st>%s<br>&lt;&lt;server&lt;&lt;</st><br>", str_s.c_str());
-				}
-				for (; (*pc)  &&  (*pc)<32; pc++) {}
-				for (; (*ps)  &&  (*ps)<32; ps++) {}
-				mc = pc;
-				ms = ps;
-				eol = false;
-				equal = true;
-			}
-			
-			if (*pc) pc++;
-			if (*ps) ps++;
-		}
 	}
+	nwc_debug_t *nwd = NULL;
+	// wait for nwc_debug_t, ignore other commands
+	for(uint8 i=0; i<5; i++) {
+		network_command_t* nwc = network_check_activity( NULL, 10000 );
+		if (nwc  &&  nwc->get_id() == NWC_DEBUG) {
+			nwd = (nwc_debug_t*)nwc;
+			break;
+		}
+		delete nwc;
+	}
+	if(nwd == NULL) {
+		buf.append("ERR: server did not respond<br>\n");
+		return;
+	}
+	buf.printf("<br><h1>Log message of sync-step %d</h1><br>", first_failed->sync_step);
+	// pointer to start of current messages
+	const char* mc = (const char*)(first_failed->buf);
+	const char* ms = (const char*)(*nwd->pbuf);
+	// now loop through the strings
+	const char* pc = mc;
+	const char* ps = ms;
+	bool equal = true;
+	bool eol = false;
+	while((*pc)  ||  (*ps)) {
+		// compare
+		if (equal  &&  (*ps)!=(*pc)) {
+			equal = false;
+		}
+		// advance till end of string / end of line if necessary
+		if (*ps  &&  *ps < 32) {
+			eol = true;
+			for (; (*pc)  &&  (*pc)>=32; pc++) {}
+		}
+		else if  (*pc  &&  *pc < 32) {
+			eol = true;
+			for (; (*ps)  &&  (*ps)>=32; ps++) {}
+		}
+		// output
+		if (eol) {
+			if (equal) {
+				std::string str_c(mc, pc-mc);
+				buf.printf("%s<br>", str_c.c_str());
+			}
+			else {
+				std::string str_c(mc, pc-mc);
+				buf.printf("<em>&gt;&gt;client&gt;&gt;</em><br><em>%s</em><br>", str_c.c_str());
+				std::string str_s(ms, ps-ms);
+				buf.printf("====<br><st>%s<br>&lt;&lt;server&lt;&lt;</st><br>", str_s.c_str());
+			}
+			for (; (*pc)  &&  (*pc)<32; pc++) {}
+			for (; (*ps)  &&  (*ps)<32; ps++) {}
+			mc = pc;
+			ms = ps;
+			eol = false;
+			equal = true;
+		}
+		
+		if (*pc) pc++;
+		if (*ps) ps++;
+	}
+
+	// finally put everything into a log-file
+	chdir( umgebung_t::user_dir );
+	const char* filename = "desync.log";
+	if (FILE *f = fopen(filename, "wb")) {
+		fprintf(f, "Check failed at sync-step=%d\n", check_failed_sync_step);
+		if (last_success) {
+			fprintf(f, "Last identic checksum at sync-step=%d\n", last_success->sync_step);
+			fprintf(f, "-- Client log --\n%s\n", (const char*)last_success->buf);
+		}
+		fprintf(f, "First failed checksum at sync-step=%d\n", first_failed->sync_step);
+		fprintf(f, "-- Client log --\n%s\n", (const char*)first_failed->buf);
+		fprintf(f, "-- Server log --\n%s\n", (const char*)(*nwd->pbuf));
+		fclose(f);
+		buf.printf("Wrote report to file %s<br>\n", filename);
+	}
+	else {
+		buf.printf("ERR: could not open file %s<br>\n", filename);
+	}
+
 	return;
 send_error:
 	buf.append("ERR: send failed. the end.<br>\n");
