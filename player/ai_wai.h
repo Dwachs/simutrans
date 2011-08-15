a4cb96c48fbc2f9dfdfd7f394e296d60bc5d5b3a
Dwachs <dwachs@gmx.net>
2011-08-15 11:46:10 +0200

    FIX: crash - initialize wraplist before bt_root

--- player/ai_wai.h
+++ player/ai_wai.h
@@ -29,6 +29,7 @@ public:
 	ai_wai_t( karte_t *welt, uint8 nr );
 
 	bool is_cash_available(sint64 money);
+
 	industry_manager_t* get_industry_manager() { return industry_manager; }
 	void set_industry_manager(industry_manager_t *im) { industry_manager = im; }
 	factory_searcher_t* get_factory_searcher() { return factory_searcher; }
@@ -36,23 +37,37 @@ public:
 
 	void notify_factory(notification_factory_t flag, const fabrik_t*);
 
+	/**
+	 * interface to deal with pointers to ingame objects
+	 * that may get deleted between calls to step()
+	 */
 	void register_wrapper(wrapper_t *, const void *);
 	void unregister_wrapper(wrapper_t *);
 	void notify_wrapper(const void *);
 
 private:
 	log_t log;
-	bt_sequential_t bt_root;
 
-	industry_manager_t* industry_manager;
-	factory_searcher_t* factory_searcher;
-
+	/**
+	 * holds safe pointers to ingame objects (factories)
+	 */
 	struct wrapper_nodes_t {
 		wrapper_nodes_t(wrapper_t *w=0, const void *p=0) : wrap(w), ptr(p) {}
 		wrapper_t *wrap;
 		const void *ptr;
 	};
 	vector_tpl<wrapper_nodes_t> wraplist;
+
+	/**
+	 * the root nodes of the tree containing all the action nodes
+	 */
+	bt_sequential_t bt_root;
+
+	/**
+	 * pointers to special nodes, do not delete them
+	 */
+	industry_manager_t* industry_manager;
+	factory_searcher_t* factory_searcher;
 public:
 	log_t& get_log() { return log; };
 };
